731e1de55be0c1d7755c2a34675a2477085ca79d
Avinash-pradhan <avinashp0383@gmail.com>
2026-02-07 20:27:16 +0530

    project updated

--- AuthProject/settings.py
+++ AuthProject/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-dev-key-change-this-in-prod")
     
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = []
 
